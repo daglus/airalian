f004fd3f8702cd73b80157b785034c3b3d13ba7c
daglus <daglus.ck.ua@gmail.com>
2019-06-11 12:41:06 +0300

    Implement transaction email, add .env variable for mailers

---
+++ config/initializers/smtp.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  address: ENV['SMTP_ADDRESS'],
+  port: ENV['SMTP_PORT'],
+  domain: ENV['SMTP_DOMAIN'],
+  user_name: ENV['SMTP_USERNAME'],
+  password: ENV['SMTP_PASSWORD'],
+  authentication: :login,
+  enable_starttls_auto: true
+}
